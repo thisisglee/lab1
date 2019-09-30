6c7687728b8134072a82d6b8b33c62cf103c65c6
Gurleen Singh <200356784@student.georgianc.on.ca>
2019-09-30 18:26:33 -0400

    lab 1

---
+++ COMP2084-MusicStore/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using COMP2084_MusicStore.Models;
+
+namespace COMP2084_MusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Addition(int x, int y) {
+            return Ok("Result = " + (x + y));
+        }
+        public IActionResult Multiplication(int x, int y)
+        {
+            return Ok("Result = " + (x * y));
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ COMP2084-MusicStore/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace COMP2084_MusicStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
